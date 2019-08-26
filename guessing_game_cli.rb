ec17d268034b00e8eedfb088f75152efee997c9d
Sergio Perez <sergio13prez@gmail.com>
2019-08-26 23:45:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,9 +4,5 @@ def run_guessing_game
   user_input = gets.chomp
   random_number = 1 + rand(6)
   
-  if user_input == "exit"
-    puts "Goodbye!"
-  elsif random_number == user_input
-    puts "You guessed the correct number!"
-  end
+
 end
