b9fbb51625bc7f05f31f367fd5eb929f6bf56aaa
Sergio Perez <sergio13prez@gmail.com>
2019-08-26 23:41:47 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,6 @@ def run_guessing_game
   if user_input == "exit"
     puts "Goodbye!"
   elsif user_input != random_number
-    puts "Sorry the computer guessed #{random_number}"
+    puts "Sorry the computer guessed #{random_number}."
   end
 end
