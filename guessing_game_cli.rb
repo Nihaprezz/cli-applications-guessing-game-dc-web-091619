5fce80d8be46528990e9dc86d70654be2eea971f
Sergio Perez <sergio13prez@gmail.com>
2019-08-26 23:30:53 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,5 +4,9 @@ def run_guessing_game
   random_number = rand(1..6)
   
   puts "Enter your guess?"
-  user_input = 
+  user_input = gets.chomp
+  
+  if user_input == 'exit'
+    print "Goodbye!"
+  end
 end
