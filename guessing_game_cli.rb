eee86cb4978c03e236c96016a372753171c9b0cb
Sergio Perez <sergio13prez@gmail.com>
2019-08-26 23:48:05 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@
 def run_guessing_game
   user_input = gets.chomp
   random_number = 1 + rand(6)
-  random_number.to_str
+  random_number.to_s
   
   puts "user input is #{user_input.class}"
   puts "random number is #{random_number.class}"
