90868d444c3caafa2b806e51bd3c9a1c240ac4c0
Sergio Perez <sergio13prez@gmail.com>
2019-08-26 23:52:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,9 +1,9 @@
 # Code your solution here!
 
 def run_guessing_game
-  user_input = gets.chomp
-  random_number = 1 + rand(6)
-  random_number = random_number.to_s
+  user_input = gets.chomp #this saves as a string 
+  random_number = 1 + rand(6) #this saves as an Integer
+  random_number = random_number.to_s #this converts the Integer into a String so we can compare the two
   
   if user_input == 'exit'
     puts "Goodbye!"
